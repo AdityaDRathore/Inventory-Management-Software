d0ee49a27d2ec2b82971f27cd13c828bf0d157c4
Aditya Rathore <153427299+AdityaDRathore@users.noreply.github.com>
2025-01-09 18:03:58 +0000

    initially setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./apps/frontend/src/**/*.{js,jsx,ts,tsx}',];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
